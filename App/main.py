56b87d2779fdbf1a7b6e45a641d59ec87d4fe6eb
barath kumar <mabarathkumar@gmail.com>
2026-02-24 19:02:18 +0530

    middleware had been added

--- App/main.py
+++ App/main.py
@@ -7,6 +7,19 @@ from App.Sms.sms import api
 
 main  = FastAPI()
 
+
+### setup the middleware for the frontend to the acess the function in the backend logic 
+
+from fastapi.middleware.cors import CORSMiddleware
+main.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 # main.include_router(router)
 main.include_router(api)
 main.include_router(VoiceRouter)
