001c89e1c3b2de4751c5a10b47317f1e50a2ae96
barath kumar <mabarathkumar@gmail.com>
2026-03-01 11:08:16 +0530

    sms features successfully connect with backend

--- App/Sms/sms.py
+++ App/Sms/sms.py
@@ -18,9 +18,8 @@ account_service = os.getenv("Twilio_Service_id")
 client = Client(account_sid,account_token)
 
 @api.post("/send_otp")
-def send_Otp(PhoneNumber:int,Username:str):
+def send_Otp(PhoneNumber:int):
     try:
-        print(Username)
         verification = client.verify.services(str(account_service)).verifications.create(
             to=f"+91{str(PhoneNumber)}", channel="sms"
         )
@@ -32,23 +31,16 @@ def send_Otp(PhoneNumber:int,Username:str):
 def verify_otp(phone: str, code: str):
     try:
         verification_check = client.verify.services(str(account_service)).verification_checks.create(
-            to=phone, code=code
+            to=f"+91{str(phone)}", code=code
         )
         return {"status": verification_check.status}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-
-
-# @api.post("/verify_otp")
-# async def verify_endpoint(phone:str=Query(...),code:str=Query(...)):
-#           result = verify_otp(phone,code)
-#           if result['status'] == "approved":
-#                   existing_user = getuserPhone(phone)    
-#                   if existing_user:
-#                        return               
-#           else:
-#                return { 
-#                 "status": result["status"],
-#                 "message": result.get("message", "OTP verification failed"),
-#                 "user_exists": False
-#             }
+    
+@api.get("/verify_otp")
+async def verify_endpoint(phone:str=Query(...),code:str=Query(...)):
+          result = verify_otp(phone,code)
+          if result['status'] == "approved":
+               return {"status":"Sucess"}
+          else:
+               return{"status":"failed"}
