2ad81084f0f34b17d9a80c9f636420e305a610b9
barath kumar <mabarathkumar@gmail.com>
2026-03-08 11:24:03 +0530

    cors middleware issue

--- App/main.py
+++ App/main.py
@@ -23,7 +23,7 @@ def hell0():
 from fastapi.middleware.cors import CORSMiddleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  
+    allow_origins=[origins],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
